54c12e4fd3f481a3a0f1166b4fd68c4609e0aa9a
molofu <484023027@qq.com>
2023-10-19 19:42:46 +0800

    修改

--- struct_address/struct_address.c
+++ struct_address/struct_address.c
@@ -14,12 +14,12 @@ struct foo {
 int main(int argc, char** argv) {
     struct foo f = { 0 };       //赋值为0  ，指针a的内容为0 ，指向了0地址
     if (f.a->s) {           //a指向的s 数组名代表地址 偏移了一个len 为4;不为0
-        printf(f.a->s);     //printf(地址) 代表打印地址的内容 字符串格式 
+       // printf(f.a->s);     //printf(地址) 代表打印地址的内容 字符串格式 
                             //类似 printf("hello world") 而此时地址为4 属于无访问权限的空间
-        
+
                             //只是无权限访问此地址的内容，但仍可以打印该地址
-        
-        printf("%x\n", &f.a->len);      
+
+        printf("%x\n", &f.a->len);
     }
     return 0;
 }
--- test/test_enter.c
+++ test/test_enter.c
@@ -6,9 +6,9 @@ void main()
 {
 
 	int x = 0;
-	scanf("%d", &x);
+	scanf("%d\n", &x);
 	printf("输出x:%d\n", x);
-	fflush(stdin);
+	fflush(stdin);		//fflush在高版本vs中已失效
 	system("pause");
 	return 0;
 }
